f9b718cc7b841c8aa8c381b5b27a1a625af9f9f2
Gulben Emiroglu <62151539+gulbenemiroglu@users.noreply.github.com>
2023-01-08 05:57:05 +0300

    Entity Framework integration on data access

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // generic constraint
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        // p=> p.id==2 gibi filtreleme yapmamızı sağlar. filter=null filtre yazmasak da olur demektir.
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
